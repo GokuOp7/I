64e65e48795fbfdfe875966f12ed52ffa4689177
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-12-10 19:44:42 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -28,30 +28,28 @@ logging.getLogger("pyrogram").setLevel(logging.INFO)
 logging.getLogger("urllib3").setLevel(logging.INFO)
 LOGS = logging.getLogger(__name__)
 
-
 THUMB = "https://telegra.ph/file/1c2a8f45940e4cb43dbeb.jpg"
 os.system(f"wget {THUMB} -O thumb.jpg")
 ffmpeg = []
 ffmpeg.append("-map 0 -c:v libx265 -crf 28 -c:s copy  -s 852x480 -preset veryfast -c:a libopus -ab 45k -vbr 2 -ac 2 -level 2.1")
 try:
- api_id = int(os.environ.get("API_ID", default=3847632))
- api_hash = os.environ.get("API_HASH", default="1a9708f807ddd06b10337f2091c67657")
- bot_token = os.environ.get("BOT_TOKEN", default="6606855749:AAFqUCMIS11sLtzmbMKHlElEhk7hpsMuHD4")
- DATABASE_URL = os.environ.get("DATABASE_URL", default="mongodb+srv://personaluse:ImCrAzYbOy@personaluse.ounsjuz.mongodb.net/?retryWrites=true&w=majority") 
- BOT_USERNAME = "bot_for_learning_nameless_bot"
- MAX_MESSAGE_LENGTH = 4096
- download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
- sudo_users = list(set(int(x) for x in int(os.environ.get("SUDO_USERS", default=6748415360)).split()))
- sudo_users.append(6748415360)
- LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", default=-1002142167497))
+    api_id = int(os.environ.get("API_ID", default=3847632))
+    api_hash = os.environ.get("API_HASH", default="1a9708f807ddd06b10337f2091c67657")
+    bot_token = os.environ.get("BOT_TOKEN", default="6606855749:AAFqUCMIS11sLtzmbMKHlElEhk7hpsMuHD4")
+    DATABASE_URL = os.environ.get("DATABASE_URL", default="mongodb+srv://personaluse:ImCrAzYbOy@personaluse.ounsjuz.mongodb.net/?retryWrites=true&w=majority") 
+    BOT_USERNAME = "bot_for_learning_nameless_bot"
+    MAX_MESSAGE_LENGTH = 4096
+    download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
+    sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS", default="6748415360").split()))
+    sudo_users.append(6748415360)
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", default=-1002142167497))
 except Exception as e:
- LOGS.info("ENV Are Missing")
+    LOGS.info("ENV Are Missing")
 
 app = Client("nirusaki", api_id=api_id, api_hash=api_hash, bot_token=bot_token, workers=2)
-0
 data = []
 
 if not download_dir.endswith("/"):
-  download_dir = str(download_dir) + "/"
+    download_dir = str(download_dir) + "/"
 if not os.path.isdir(download_dir):
-  os.makedirs(download_dir)
+    os.makedirs(download_dir)
